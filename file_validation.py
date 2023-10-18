347f1479568d1c73aa76b0557d167e0d89562270
sahdevgrover <sahdevgrover02@gmail.com>
2023-10-18 23:14:05 +0530

    2nd commit

---
+++ file_validation.py
@@ -0,0 +1,39 @@
+import boto3
+import json
+import pandas as pd
+
+s3_client = boto3.client('s3')
+sns_client = boto3.client('sns')
+sqs_client = boto3.client('sqs')
+
+def lambda_handler(event, context):
+
+    print(event)
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
+
+
+    download_path = '/tmp/{}'.format(key)
+    s3_client.download_file(bucket, key, download_path)
+
+
+    df = pd.read_excel(download_path)
+
+    expected_columns = ["Id", "Name", "Address"]
+    actual_columns = df.columns.tolist()
+
+    if set(expected_columns) != set(actual_columns):
+        # Send SNS notification
+        sns_message = f"File {key} is not proper. Expected columns: {expected_columns}, Actual columns: {actual_columns}"
+        sns_client.publish(TopicArn='arn:aws:sns:us-east-1:865935148049:key-reminder', Message=sns_message)
+        print('Email Send')
+    else:
+        # Send SQS message
+        sqs_message = {'file_name': key}
+        sqs_client.send_message(QueueUrl='https://sqs.us-east-1.amazonaws.com/865935148049/test', MessageBody=json.dumps(sqs_message))
+        print('Message publish to ')
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('File processing complete.')
+    }
